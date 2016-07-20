30b94679d6f453dc98b3ce2d618b1252bf6b8584
Christian Neukirchen <chneukirchen@gmail.com>
2016-07-20 13:46:39 +0200

    mseq: -n to expand ranges

--- mseq.c
+++ mseq.c
@@ -4,9 +4,20 @@
 
 #include "blaze822.h"
 
+static int nflag;
+
 int
 main(int argc, char *argv[])
 {
+	int c;
+	while ((c = getopt(argc, argv, "n")) != -1)
+		switch(c) {
+		case 'n': nflag = 1; break;
+		default:
+			// XXX usage
+			exit(1);
+		}
+
 	char *map = blaze822_seq_open(0);
 	if (!map)
 		return 1;
@@ -14,9 +25,12 @@ main(int argc, char *argv[])
 	int i;
 	char *f;
 	struct blaze822_seq_iter iter = { 0 };
-	for (i = 1; i < argc; i++) {
+	for (i = optind; i < argc; i++) {
 		while ((f = blaze822_seq_next(map, argv[i], &iter))) {
-			printf("%s\n", f);
+			if (nflag)
+				printf("%ld\n", iter.line-1);
+			else
+				printf("%s\n", f);
 			free(f);
 		}
 	}
